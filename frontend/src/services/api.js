99a3c420a9607eeb38226e4f20192e89c3f8632c
anpise <anpise@iu.edu>
2025-04-30 15:18:47 -0400

    added dockerfile

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5050/api' });
+const API = axios.create({ baseURL: `${process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000'}/api` });
 
 API.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
